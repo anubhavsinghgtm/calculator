8b2ff95cf86257975e9a23abf69d308429c5587a
Anubhav Singh <anubhavsinghgtm@gmail.com>
2024-04-24 17:23:08 +0530

    double operator error resolved

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ let firstVar = 0;
 let secondVar = 0;
 let currAns = 0;
 let operation;
+let isFirstSet = false;
+let isOperator = false;
 
 
 const setScreenAns = (sceenAns) => {
+    // if(isNaN(sceenAns)){
+    //     sceenAns = 0;
+    // }
     screenArea.innerHTML = sceenAns;
+    console.log(sceenAns);
 }
 
 
@@ -23,6 +29,7 @@ const findSecondVar = () => {
 
 const findFirstVar = () => {
     // return first var
+    isFirstSet = true;
     return Number(screenAns);
 }
 
@@ -36,33 +43,44 @@ const calculateAns = () => {
     else if(operation == '*'){
         return firstVar * secondVar;
     }
-    else if(operation == '/' && secondVar === '0'){
+    else if(operation == '/' && secondVar == '0'){
         console.log("Denominator can't be zero")
+        return 0;
     }
     else if(operation == '/'){
         return firstVar/secondVar;
     }
+
+}
+
+
+const setDefault = () => {
+    firstVar = 0;
+    secondVar = 0;
+    isFirstSet = false;
 }
 
 
 keys.forEach((key) => {
     key.addEventListener('click', () => {
         if(key.innerHTML == 'CLS'){
-            screenAns = '';
+            screenAns = '0';
             screenArea.innerHTML = '0'
         }
 
-        else if(key.innerHTML == '='){
+        else if(key.innerHTML == '=' && isFirstSet == true){
             secondVar = findSecondVar();
             screenAns = calculateAns();
+            setDefault();
         }
 
         else{
             screenAns += key.innerHTML;
         }
 
-        if(screenAns != 0)
-            setScreenAns(screenAns);
+        // if(screenAns != 0)
+        setScreenAns(screenAns);
+        isOperator = false;
 
         
     })
@@ -72,10 +90,29 @@ keys.forEach((key) => {
 operators.forEach((operator) => {
     operator.addEventListener('click', () => {
         
-        firstVar = findFirstVar();
+        // const index = screenAns.indexOf(operation);
+        // console.log(index);
+
+
+        if(isFirstSet == true && isOperator == false){
+            secondVar = findSecondVar();
+            screenAns = calculateAns();
+            
+        }
+
+        if(isOperator == false){
+            firstVar = findFirstVar();
+        }
+        
         operation = operator.innerHTML;
+
+        if(isOperator == true){
+            screenAns = screenAns.substring(0, screenAns.length - 1);
+        }
 
         screenAns += operation;
         setScreenAns(screenAns);
+
+        isOperator = true;
     })
 })
